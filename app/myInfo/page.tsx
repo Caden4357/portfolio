1c5d39033fe739e030252b2b1996db99a6c7c5d1
Caden Wilcox <wilcox4357@gmail.com>
2023-08-02 14:07:17 -0700

    removed resume button temporarily

--- app/myInfo/page.tsx
+++ app/myInfo/page.tsx
@@ -12,7 +12,6 @@ export default function MyInfo() {
                     <h1 className={styles.textPop}><span>Caden</span> Wilcox</h1>
                     <h2>Full Stack Developer</h2>
                     <p>From code to creativity, I build digital dreams with a full-stack flair</p>
-                    <button className={`${styles.textPop} ${styles.resume}`}>Resume</button>
                     <Link target="_blank" href="https://www.linkedin.com/in/caden-wilcox-python/"><img className={styles.smIcon} src="/linkedIn.png" alt="" /></Link>
                     <div className='d-flex jc-center ai-center gap'>
                         <h3><Link target="_blank" className={styles.link} href='mailto:wilcox4357@gmail.com'>Contact Me!</Link></h3>
